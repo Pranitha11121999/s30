e898b01f498d06e9ac991f0b29d0e3e265cc981c
pranitha <kpranithaharani@gmail.com>
2025-12-28 19:46:16 -0800

    Problem 3

---
+++ Binary-Search-2/find-peak-element.py
@@ -0,0 +1,14 @@
+"""We need to climb towards the peak always no matter what"""
+
+class Solution:
+    def findPeakElement(self, nums: List[int]) -> int:
+        low, high = 0, len(nums)-1
+        while low <= high:
+            mid = low + (high-low)//2
+            if (mid==0 or nums[mid]>nums[mid-1]) and (mid ==len(nums)-1 or nums[mid]>nums[mid+1]):
+                return mid
+            elif nums[mid]<nums[mid+1]:
+                low = mid+1
+            else:
+                high = mid -1
+        return -1
